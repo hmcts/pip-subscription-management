ec94381b5998f102fa0469a664937b06f4ffe1c4
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2021-07-20 15:11:06 +0100

    PUB-503 - Tests for Global Exception Handler (#8)
    * Added Tests for Global Exception Handling

--- src/main/java/uk/gov/hmcts/reform/demo/errorhandling/GlobalExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/reform/demo/errorhandling/GlobalExceptionHandler.java
@@ -24,7 +24,8 @@ public class GlobalExceptionHandler {
      * @return The error response, modelled using the ExceptionResponse object.
      */
     @ExceptionHandler(SubscriptionNotFoundException.class)
-    ResponseEntity<ExceptionResponse> handleSubscriptionNotFound(SubscriptionNotFoundException ex, WebRequest request) {
+    public ResponseEntity<ExceptionResponse> handleSubscriptionNotFound(
+        SubscriptionNotFoundException ex, WebRequest request) {
 
         ExceptionResponse exceptionResponse = new ExceptionResponse();
         exceptionResponse.setMessage(ex.getMessage());
