a16636d630556f1cb5825997e9ea8f4f7ed4c2f0
Kurt Plaschke <k.plaschke@hull.ac.uk>
2021-07-23 13:46:04 +0100

    moved '+' onto new line to satisfy checkstyle

--- src/main/java/uk/gov/hmcts/reform/demo/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/demo/config/SwaggerConfiguration.java
@@ -30,8 +30,8 @@ public class SwaggerConfiguration {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
             .title("P&I Subscription Management Service")
-            .description("Use this service to view and manage the subscriptions against a publication(s). You can " +
-                             "find out more about how the code works at https://github.com/hmcts/pip-subscription-management")
+            .description("Use this service to view and manage the subscriptions against a publication(s). You can "
+                             + "find out more about how the code works at https://github.com/hmcts/pip-subscription-management")
             .version("1.0.0")
             .contact(new Contact("P&I Team",
                                  "https://tools.hmcts.net/confluence/display/PUBH/Publication+Hub",
