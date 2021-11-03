71ed0cc7954321e8d661403009823b929e934c1a
Christopher Pateman <christopher.pateman@version1.com>
2021-11-03 10:00:44 +0000

    update references and update variables

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,7 +2,6 @@ locals {
   product                  = "pip"
   component                = "subecription-mgmt"
   builtFrom                = "hmcts/jenkins/subscription-management"
-  common_tags              = module.ctags.common_tags
   resource_group_name      = "pip-sharedinfra-${var.env}-rg"
   storage_account_name     = "pipsharedinfrasa${var.env}"
   dtu_storage_account_name = "pipdtu${var.env}"
@@ -13,13 +12,6 @@ locals {
   postgresql_prefix        = "postgre"
 }
 
-module "ctags" {
-  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
-  environment = var.env
-  product     = local.product
-  builtFrom   = local.builtFrom
-}
-
 data "azurerm_subnet" "postgres" {
   name                 = "iaas"
   resource_group_name  = "ss-${var.env}-network-rg"
@@ -28,7 +20,7 @@ data "azurerm_subnet" "postgres" {
 
 module "databases" {
   for_each           = { for database in var.databases : database => database }
-  source             = "git::https://github.com/hmcts/cnp-module-postgres.git?ref=postgresql_tf"
+  source             = "git@github.com/hmcts/cnp-module-postgres.git?ref=postgresql_tf"
   product            = local.product
   component          = local.component
   subnet_id          = data.azurerm_subnet.postgres.id
@@ -36,7 +28,7 @@ module "databases" {
   env                = local.env_long_name
   postgresql_user    = local.postgresql_user
   database_name      = each.value
-  common_tags        = local.common_tags
+  common_tags        = var.common_tags
   subscription       = local.env_long_name
   business_area      = "SDS"
   postgresql_version = 10
@@ -49,52 +41,49 @@ data "azurerm_key_vault" "ss_kv" {
   name                = "${local.product}-shared-kv-${var.env}"
   resource_group_name = "${local.product}-sharedservices-${var.env}-rg"
 }
-module "keyvault_postgre_secrets" {
-  for_each = { for database in module.databases : database.name => database }
-  source   = "./modules/key-vault/secret"
 
-  key_vault_id = data.azurerm_key_vault.ss_kv.id
-  tags         = local.common_tags
-  secrets = [
-    {
-      name  = "${local.postgresql_prefix}-${each.value.postgresql_database}-host"
-      value = each.value.host_name
-      tags = {
-        "source" : "PostgreSQL"
-      }
-      content_type = ""
-    },
-    {
-      name  = "${local.postgresql_prefix}-${each.value.postgresql_database}-port"
-      value = each.value.postgresql_listen_port
-      tags = {
-        "source" : "PostgreSQL"
-      }
-      content_type = ""
-    },
-    {
-      name  = "${local.postgresql_prefix}-${each.value.postgresql_database}-user"
-      value = each.value.user_name
-      tags = {
-        "source" : "PostgreSQL"
-      }
-      content_type = ""
-    },
-    {
-      name  = "${local.postgresql_prefix}-${each.value.postgresql_database}-pwd"
-      value = each.value.postgresql_password
-      tags = {
-        "source" : "PostgreSQL"
-      }
-      content_type = ""
-    },
-    {
-      name  = "${local.postgresql_prefix}-${each.value.postgresql_database}-name"
-      value = each.value.postgresql_database
-      tags = {
-        "source" : "PostgreSQL"
-      }
-      content_type = ""
-    }
-  ]
+resource "azurerm_key_vault_secret" "db-host" {
+  for_each = { for database in module.databases : database.name => database }
+  key_vault_id    = data.azurerm_key_vault.ss_kv.id
+  name            = "${local.postgresql_prefix}-${each.value.postgresql_database}-host"
+  value           = each.value.host_name
+  tags            = merge(var.common_tags, {"source" : "PostgreSQL"})
+  content_type    = ""
+  expiration_date = timeadd(timestamp(), "8760h")
+}
+resource "azurerm_key_vault_secret" "db-port" {
+  for_each = { for database in module.databases : database.name => database }
+  key_vault_id    = data.azurerm_key_vault.ss_kv.id
+  name            = "${local.postgresql_prefix}-${each.value.postgresql_database}-port"
+  value           = each.value.postgresql_listen_port
+  tags            = merge(var.common_tags, {"source" : "PostgreSQL"})
+  content_type    = ""
+  expiration_date = timeadd(timestamp(), "8760h")
+}
+resource "azurerm_key_vault_secret" "db-user" {
+  for_each = { for database in module.databases : database.name => database }
+  key_vault_id    = data.azurerm_key_vault.ss_kv.id
+  name            = "${local.postgresql_prefix}-${each.value.postgresql_database}-user"
+  value           = each.value.user_name
+  tags            = merge(var.common_tags, {"source" : "PostgreSQL"})
+  content_type    = ""
+  expiration_date = timeadd(timestamp(), "8760h")
+}
+resource "azurerm_key_vault_secret" "db-pwd" {
+  for_each = { for database in module.databases : database.name => database }
+  key_vault_id    = data.azurerm_key_vault.ss_kv.id
+  name            = "${local.postgresql_prefix}-${each.value.postgresql_database}-pwd"
+  value           = each.value.postgresql_password
+  tags            = merge(var.common_tags, {"source" : "PostgreSQL"})
+  content_type    = ""
+  expiration_date = timeadd(timestamp(), "8760h")
+}
+resource "azurerm_key_vault_secret" "db-name" {
+  for_each = { for database in module.databases : database.name => database }
+  key_vault_id    = data.azurerm_key_vault.ss_kv.id
+  name            = "${local.postgresql_prefix}-${each.value.postgresql_database}-name"
+  value           = each.value.postgresql_database
+  tags            = merge(var.common_tags, {"source" : "PostgreSQL"})
+  content_type    = ""
+  expiration_date = timeadd(timestamp(), "8760h")
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,16 +1,25 @@
+variable "product" {}
+
+variable "component" {}
 
 variable "location" {
-  type        = string
-  default     = "UK South"
-  description = "Deployment location"
-}
-variable "env" {
-  type        = string
-  description = "Deployment Environment"
+  default = "UK South"
 }
 
+variable "env" {}
+
+variable "subscription" {}
+
+variable "deployment_namespace" {}
+
+variable "common_tags" {
+  type = "map"
+}
+
+
+#Databases
 variable "databases" {
   type        = list(string)
   description = "List of Databases Names to create"
-  default     = ["subscriptions"]
+  default     = []
 }
