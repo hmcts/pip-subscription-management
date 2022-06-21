8c7fcdc1c480e2b6d93cf6b85adc657ba167e9b3
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2022-06-21 08:36:02 +0100

    Merge integration into master (#60)
    * Pub 1272 subscribe by list type (#55)
    
    * initial commit - altered the collectSubscribers endpoint
    
    * connection to channel-management built
    
    * Push for Josh
    
    * Work in progress
    
    * Refactored code and connected up to publication services
    
    * checkstyle/PMD cleanup
    
    * Fixed a bug, unit tests in progress:
    
    * Added supression for Netty - only impacts Java 6 and under
    
    * More tests added
    
    * More unit tests written
    
    * Read me change to trigger build
    
    * Added suppression
    
    * Review comments
    
    * Updated spring boot to 2.5.14, removed netty supression
    
    * Updated tomcat
    
    * Updated spring version in supressions
    
    * Updated spring version in supressions
    
    * Updated channel management endpoint
    
    * added sub by list to options and collected api subs
    
    * added suppression after checking upgrade doesnt resolve
    
    * updated API channel to API_COURTEL and added a route value to the enum
    
    * resolved pmd issues
    
    Co-authored-by: Daniel Furnivall <danielfurnivall@gmail.com>
    Co-authored-by: Josh Blackmoor <joshblackmoor01@icloud.com>
    
    * PUB-1356 - Subscription Flow (#57)
    
    * Added in security for service-to-service and suppressions
    
    * Bumping chart version
    
    * Added config
    
    * Fixed integration tests and updated config
    
    * Updated supressions
    
    * Updated gradle
    
    * Updated test
    
    * Updated int tests to adhere to order
    
    * Fixed unused imports
    
    Co-authored-by: hmcts-jenkins-cnp <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>
    Co-authored-by: ChrisS1512 <Christopher.Stacey@justice.gov.uk>
    
    * Pub-949 outbound api (#58)
    
    * initial commit - altered the collectSubscribers endpoint
    
    * connection to channel-management built
    
    * Push for Josh
    
    * Work in progress
    
    * Refactored code and connected up to publication services
    
    * checkstyle/PMD cleanup
    
    * Fixed a bug, unit tests in progress:
    
    * Added supression for Netty - only impacts Java 6 and under
    
    * More tests added
    
    * More unit tests written
    
    * Read me change to trigger build
    
    * Added suppression
    
    * Review comments
    
    * Updated spring boot to 2.5.14, removed netty supression
    
    * Updated tomcat
    
    * Updated spring version in supressions
    
    * Updated spring version in supressions
    
    * Updated channel management endpoint
    
    * added sub by list to options and collected api subs
    
    * added suppression after checking upgrade doesnt resolve
    
    * updated API channel to API_COURTEL and added a route value to the enum
    
    * resolved pmd issues
    
    * added 949 api calls to channel management and publication services
    
    * checkstyle changes
    
    * updated api and changed sorting value to enum param
    
    * added suppression
    
    * removed incorrect puyb services payload
    
    * added attribute to websclient
    
    Co-authored-by: Daniel Furnivall <danielfurnivall@gmail.com>
    Co-authored-by: Josh Blackmoor <joshblackmoor01@icloud.com>
    
    Co-authored-by: Kurt <kurtplaschke@hotmail.co.uk>
    Co-authored-by: Daniel Furnivall <danielfurnivall@gmail.com>
    Co-authored-by: Josh Blackmoor <joshblackmoor01@icloud.com>
    Co-authored-by: Josh <joshblackmoor@users.noreply.github.com>
    Co-authored-by: hmcts-jenkins-cnp <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/pip/subscription/management/service/ChannelManagementService.java
+++ src/main/java/uk/gov/hmcts/reform/pip/subscription/management/service/ChannelManagementService.java
@@ -8,17 +8,21 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import uk.gov.hmcts.reform.pip.subscription.management.models.Subscription;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
+
 @Slf4j
 @Component
 public class ChannelManagementService {
 
     private static final String EMAIL_PATH = "/channel/emails";
+    private static final String API_PATH = "/channel/api";
 
     @Autowired
     private WebClient webClient;
@@ -29,6 +33,7 @@ public class ChannelManagementService {
     public Map<String, List<Subscription>> getMappedEmails(List<Subscription> listOfSubs) {
         try {
             return webClient.post().uri(url + EMAIL_PATH)
+                .attributes(clientRegistrationId("channelManagementApi"))
                 .body(BodyInserters.fromValue(listOfSubs))
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<Map<String, List<Subscription>>>() {})
@@ -36,6 +41,19 @@ public class ChannelManagementService {
         } catch (WebClientException ex) {
             log.error(String.format("Request with body: %s failed. With error message: %s",
                                     listOfSubs, ex.getMessage()));
+            return Collections.emptyMap();
+        }
+    }
+
+    public Map<String, List<Subscription>> getMappedApis(List<Subscription> subscriptions) {
+        try {
+            return webClient.post().uri(url + API_PATH)
+                .attributes(clientRegistrationId("channelManagementApi"))
+                .bodyValue(subscriptions)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Map<String, List<Subscription>>>() {}).block();
+        } catch (WebClientResponseException ex) {
+            log.error("Request to Channel Management {} failed due to: {}", API_PATH, ex.getResponseBodyAsString());
             return Collections.emptyMap();
         }
     }
