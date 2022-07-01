2d54dac5495bf9f8cb31449e589b85e035bc98a1
furnivall <danielfurnivall@gmail.com>
2022-07-01 17:49:21 +0100

    log4j new suppression (#63)
    * supp
    
    * suppressions for other stuff
    
    * attempt 2
    
    * added smoke test

---
+++ src/smokeTest/java/uk/gov/hmcts/reform/pip/subscription/management/TestClass.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.reform.pip.subscription.management;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestClass {
+
+    /**
+     * Placeholder for Smoke Tests.
+     */
+    @Test
+    void placeholderTest() {
+        assertEquals("Placeholder Test", "Placeholder Test", "Placeholder test for smoke tests");
+    }
+
+}
+
