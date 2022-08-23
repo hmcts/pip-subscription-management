29bce9a413fa5ddb5b18530c58d85d8fd827fe25
Daniel Furnivall <danielfurnivall@gmail.com>
2022-08-23 11:38:54 +0100

    fixes for sonar

--- src/test/java/uk/gov/hmcts/reform/pip/subscription/management/service/SubscriptionServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/subscription/management/service/SubscriptionServiceTest.java
@@ -75,6 +75,15 @@ class SubscriptionServiceTest {
     private static final String TEST_USER_EMAIL = "a@b.com";
     private static final String SUCCESS = "Success";
     private static final String TEST = "test";
+    public static final List<String> EXAMPLE_CSV_ALL = List.of(
+        "a01d52c0-5c95-4f75-8994-a1c42cb45aaa,EMAIL,CASE_ID,2fe899ff-96ed-435a-bcad-1411bbe96d2a,string",
+        "370963e2-9d2f-423e-b6a1-3f1f8905cdf0,EMAIL,CASE_ID,2fe899ff-96ed-435a-bcad-1411bbe96d2a,1234",
+        "052cda55-30fd-4a0d-939a-2c7b03ab3392,EMAIL,CASE_ID,2fe899ff-96ed-435a-bcad-1411bbe96d2a,1234"
+        );
+    public static final List<String> EXAMPLE_CSV_LOCAL = List.of(
+        "212c8b34-f6c3-424d-90e2-f874f528eebf,2,EMAIL,2fe899ff-96ed-435a-bcad-1411bbe96d2a,null",
+        "f4a0cb33-f211-4b46-8bdb-6320f6382a29,1234,API,2fe899ff-96ed-435a-bcad-1411bbe96d2a,null",
+        "34edfcde-4546-46b8-98e6-2717da3185e8,3,API,2fe899ff-96ed-435a-bcad-1411bbe96d2a,Oxford Combined Court Centre");
 
     private List<Subscription> mockSubscriptionList;
     private Subscription mockSubscription;
@@ -530,16 +539,18 @@ class SubscriptionServiceTest {
     }
 
     @Test
-    void testMiEndpointLocal() {
+    void testMiServiceLocal() {
+        when(subscriptionRepository.getLocalSubsDataForMi()).thenReturn(EXAMPLE_CSV_LOCAL);
         String testString = subscriptionService.getLocalSubscriptionsDataForMiReporting();
         String[] splitLineString = testString.split("\r\n|\r|\n");
         long countLine1 = splitLineString[0].chars().filter(character -> character == ',').count();
         assertThat(testString)
             .as("Header row missing")
             .contains("user_id");
-        assertThat(splitLineString)
-            .as("Only one line exists - data must be missing, as only headers are printing")
-            .hasSizeGreaterThanOrEqualTo(1);
+        assertThat(testString)
+            .as("Json parsing has probably failed")
+            .contains("Oxford")
+            .hasLineCount(4);
         assertThat(splitLineString)
             .as("Wrong comma count compared to header row!")
             .allSatisfy(
@@ -548,16 +559,18 @@ class SubscriptionServiceTest {
     }
 
     @Test
-    void testMiEndpointAll() {
+    void testMiServiceAll() {
+        when(subscriptionRepository.getAllSubsDataForMi()).thenReturn(EXAMPLE_CSV_LOCAL);
         String testString = subscriptionService.getAllSubscriptionsDataForMiReporting();
         String[] splitLineString = testString.split("\r\n|\r|\n");
         long countLine1 = splitLineString[0].chars().filter(character -> character == ',').count();
         assertThat(testString)
+            .as("Json parsing has probably failed")
+            .contains("Oxford")
+            .hasLineCount(4);
+        assertThat(testString)
             .as("Header row missing")
             .contains("user_id");
-        assertThat(splitLineString)
-            .as("Only one line exists - data must be missing, as only headers are printing")
-            .hasSizeGreaterThanOrEqualTo(1);
         assertThat(splitLineString)
             .as("Wrong comma count compared to header row!")
             .allSatisfy(
