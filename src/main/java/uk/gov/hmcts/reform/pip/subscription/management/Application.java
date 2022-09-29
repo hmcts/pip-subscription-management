29f1625fbb0d5194a4eaf102a75a8a5376867725
Josh Blackmoor <joshblackmoor01@icloud.com>
2022-09-29 11:35:14 +0100

    clean up

--- src/main/java/uk/gov/hmcts/reform/pip/subscription/management/Application.java
+++ src/main/java/uk/gov/hmcts/reform/pip/subscription/management/Application.java
@@ -1,11 +1,9 @@
 package uk.gov.hmcts.reform.pip.subscription.management;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
