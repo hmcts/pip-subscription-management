cbea08dedec9fb8ad3edf553a29d768d60d3512c
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2022-11-10 17:56:00 +0000

    Creates the infrastructure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,6 +4,7 @@ locals {
   resource_group_name = "${local.prefix}-${var.env}-rg"
   key_vault_name      = "${local.prefix}-kv-${var.env}"
   env_long_name       = var.env == "sbox" ? "sandbox" : var.env == "stg" ? "staging" : var.env
+  sdp_key_vault_name  = "${local.prefix}-sdp-kv-${var.env}"
 }
 data "azurerm_client_config" "current" {}
 
@@ -19,3 +20,17 @@ data "azurerm_key_vault" "kv" {
   resource_group_name = local.resource_group_name
 }
 
+data "azurerm_key_vault" "sdp-kv" {
+  name                = local.sdp_key_vault_name
+  resource_group_name = local.resource_group_name
+}
+
+data "azurerm_key_vault_secret" "sdp-user" {
+  name         = "subscription-management-SDP-USER"
+  key_vault_id = data.azurerm_key_vault.sdp-kv.id
+}
+
+data "azurerm_key_vault_secret" "sdp-pass" {
+  name         = "subscription-management-SDP-PASS"
+  key_vault_id = data.azurerm_key_vault.sdp-kv.id
+}
