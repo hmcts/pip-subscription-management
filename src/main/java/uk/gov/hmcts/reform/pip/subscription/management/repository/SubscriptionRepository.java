f0ba85892b1370ecab3c972618f8ef0e9fe8ae79
KianKwa <kian.kwa@justice.gov.uk>
2022-11-11 08:36:58 +0000

    Endpoint for bulk delete subscriptions

--- src/main/java/uk/gov/hmcts/reform/pip/subscription/management/repository/SubscriptionRepository.java
+++ src/main/java/uk/gov/hmcts/reform/pip/subscription/management/repository/SubscriptionRepository.java
@@ -21,12 +21,16 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Long
 
     Optional<Subscription> findById(UUID id);
 
+    List<Subscription> findByIdIn(List<UUID> id);
+
     List<Subscription> findByUserId(String userId);
 
     @Override
     List<Subscription> findAll();
 
     void deleteById(UUID id);
+
+    void deleteByIdIn(List<UUID> id);
 
     @Query(value = "SELECT * FROM Subscription "
         + "WHERE search_type = :search_type "
