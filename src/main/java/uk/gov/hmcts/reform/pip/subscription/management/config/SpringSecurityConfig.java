6bc012ae225777a2bd6773d9a8ba6ea85a674fc9
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2023-06-29 18:18:43 +0100

    PUB-1687 - Updated gradle, removed old init file

--- src/main/java/uk/gov/hmcts/reform/pip/subscription/management/config/SpringSecurityConfig.java
+++ src/main/java/uk/gov/hmcts/reform/pip/subscription/management/config/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.pip.subscription.management.config;
 
 import com.azure.spring.cloud.autoconfigure.implementation.aad.security.AadResourceServerHttpSecurityConfigurer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration(proxyBeanMethods = false)
 @EnableWebSecurity
 @EnableMethodSecurity
 @Profile("!dev")
