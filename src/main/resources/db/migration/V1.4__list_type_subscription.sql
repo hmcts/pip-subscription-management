6202576912bc34388c9de82828a7d7f41dd61327
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2024-10-03 09:42:45 +0100

    fix script

--- src/main/resources/db/migration/V1.4__list_type_subscription.sql
+++ src/main/resources/db/migration/V1.4__list_type_subscription.sql
@@ -3,25 +3,18 @@
 -- subscriptions which already exists in database
 --
 
-INSERT INTO subscription_list_type(id, user_id, location_id, list_type, list_language)
-SELECT gen_random_uuid () id, user_id, CAST(search_value AS Integer) locationId, list_type, STRING_TO_ARRAY('ENGLISH,WELSH', ',') list_language
+INSERT INTO subscription_list_type(id, user_id, list_type, list_language)
+SELECT gen_random_uuid () id, user_id, list_type, STRING_TO_ARRAY('ENGLISH,WELSH', ',') list_language
 FROM
   (
-    SELECT user_id, search_value, list_type
+    SELECT user_id, list_type
     FROM subscription
     WHERE search_type = 'LOCATION_ID'
       AND list_type != '{}'
-    GROUP BY user_id, search_value, list_type
-    ORDER BY user_id, search_value
+    GROUP BY user_id, list_type
+    ORDER BY user_id
 
   )tbl
-
---
--- This script will delete list_type column from subscription table
--- because we have moved the data into new table subscription_list_type
---
-ALTER TABLE subscription
-DROP COLUMN list_type;
 
 
 
