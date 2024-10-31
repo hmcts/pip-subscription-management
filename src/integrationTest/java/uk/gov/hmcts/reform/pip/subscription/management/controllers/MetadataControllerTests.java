e779bb2e00cdcdb9ae3fc2a8c1147d092f591164
Kian Kwa <kian.kwa@justice.gov.uk>
2024-10-31 17:21:59 +0000

    Move functional tests into integration test folder

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/subscription/management/controllers/MetadataControllerTests.java
@@ -0,0 +1,75 @@
+package uk.gov.hmcts.reform.pip.subscription.management.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import uk.gov.hmcts.reform.pip.model.subscription.Channel;
+import uk.gov.hmcts.reform.pip.subscription.management.Application;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = {Application.class},
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("integration")
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@WithMockUser(username = "admin", authorities = {"APPROLE_api.request.admin"})
+@AutoConfigureEmbeddedDatabase(type = AutoConfigureEmbeddedDatabase.DatabaseType.POSTGRES)
+@SuppressWarnings({"PMD.ExcessiveImports", "PMD.LawOfDemeter"})
+class MetadataControllerTests {
+    private static final String FORBIDDEN_STATUS_CODE = "Status code does not match forbidden";
+    private static final String ROOT_URL = "/meta";
+
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    protected MockMvc mvc;
+
+    @BeforeEach
+    void setup() {
+        objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+    }
+
+    @DisplayName("Get all channels")
+    @Test
+    void testGetAllChannels() throws Exception {
+        MvcResult response = mvc.perform(get(ROOT_URL + "/channels"))
+            .andExpect(status().isOk()).andReturn();
+
+        Channel[] channelsReturned = objectMapper.readValue(
+            response.getResponse().getContentAsString(),
+            Channel[].class
+        );
+        assertEquals(Channel.values().length, channelsReturned.length,
+                     "Correct number of channels should return"
+        );
+        assertArrayEquals(Channel.values(), channelsReturned, "Channels should match");
+    }
+
+    @Test
+    @WithMockUser(username = "unauthorized_delete", authorities = {"APPROLE_unknown.delete"})
+    void testUnauthorizedGetAllChannels() throws Exception {
+        MvcResult mvcResult = mvc.perform(get(ROOT_URL + "/channels"))
+            .andExpect(status().isForbidden()).andReturn();
+
+        assertEquals(HttpStatus.FORBIDDEN.value(), mvcResult.getResponse().getStatus(),
+                     FORBIDDEN_STATUS_CODE
+        );
+    }
+}
