130543d380dfd4698d6b1d856dd3ff77de72373e
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2024-12-04 15:22:07 +0000

    fix the build

--- src/main/resources/db/migration/V1.4__list_type_subscription.sql
+++ src/main/resources/db/migration/V1.4__list_type_subscription.sql
@@ -5,7 +5,7 @@
 --CREATE EXTENSION pgcrypto;
 
 INSERT INTO subscription_list_type(id, user_id, list_type, list_language)
-SELECT gen_random_uuid () id, user_id, list_type, STRING_TO_ARRAY('ENGLISH,WELSH', ',') list_language
+SELECT user_id id, user_id, list_type, STRING_TO_ARRAY('ENGLISH,WELSH', ',') list_language
 FROM
   (
     SELECT user_id, list_type
