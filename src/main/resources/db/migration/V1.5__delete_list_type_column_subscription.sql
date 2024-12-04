507f47ccf42ad5d7c7d77f5dd4b7a3d0b9e00fc2
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2024-12-04 16:00:27 +0000

    delete script
    we need to add this script later once list type sub changes have been deployed

--- src/main/resources/db/migration/V1.5__delete_list_type_column_subscription.sql
+++
@@ -1,6 +0,0 @@
---
--- This script will delete list_type column from subscription table
--- because we have moved the data into new table subscription_list_type
---
-ALTER TABLE subscription
-DROP COLUMN list_type;
