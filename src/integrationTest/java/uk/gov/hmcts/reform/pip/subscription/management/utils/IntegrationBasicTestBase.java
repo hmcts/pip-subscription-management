ef09c28cf295c8931b04f378b62a023f8088bb31
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2025-01-14 14:15:03 +0000

    fix build

--- src/integrationTest/java/uk/gov/hmcts/reform/pip/subscription/management/utils/IntegrationBasicTestBase.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/subscription/management/utils/IntegrationBasicTestBase.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.pip.subscription.management.utils;
 
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
 import uk.gov.hmcts.reform.pip.subscription.management.repository.SubscriptionListTypeRepository;
 import uk.gov.hmcts.reform.pip.subscription.management.repository.SubscriptionRepository;
 
