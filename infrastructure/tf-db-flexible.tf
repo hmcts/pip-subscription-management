1196dd3929ae4b4c05f8bd61911192be1b78f751
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2025-04-03 14:08:31 +0100

    Updated perftest password

--- infrastructure/tf-db-flexible.tf
+++ infrastructure/tf-db-flexible.tf
@@ -28,6 +28,7 @@ module "postgresql" {
   admin_user_object_id = var.jenkins_AAD_objectId
 
   force_user_permissions_trigger = "2"
+  trigger_password_reset = "1"
 
   pgsql_server_configuration = [
     {
