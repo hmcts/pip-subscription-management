a87ceb54799486b8f40708d2a95b2398fbc91164
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2025-06-26 12:15:46 +0000

    Automated Change

--- tf-db-flexible.tf
+++ tf-db-flexible.tf
@@ -5,7 +5,7 @@ locals {
 }
 
 module "postgresql" {
-  count     = var.env == "prod" ? 1 : 0
+  count = var.env == "prod" ? 1 : 0
   providers = {
     azurerm.postgres_network = azurerm.postgres_network
   }
